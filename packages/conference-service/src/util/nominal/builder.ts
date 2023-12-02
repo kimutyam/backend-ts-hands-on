b375cc8796f657aec5b53859baf5186958f81aed
kimutyam <devkimura@gmail.com>
2023-12-02 19:52:59 +0900

    chore: 不変条件のファイルプライベートにassertのロジックを凝集

--- packages/conference-service/src/util/nominal/builder.ts
+++ packages/conference-service/src/util/nominal/builder.ts
@@ -1,6 +1,4 @@
-import assert from 'node:assert';
 import { Invariants } from './invariants';
-import type { InvariantsError } from './invariantsError';
 import type { AnyNominal, NNominal, NominalName, NominalValue } from './nominal';
 
 export interface Builder<N extends AnyNominal> {
@@ -13,8 +11,7 @@ export const Builder = <N extends AnyNominal>(
 ): Builder<N> => ({
   build: (value: NominalValue<N>): NNominal<N> => {
     if (invariants !== undefined) {
-      let error: InvariantsError<N> | undefined;
-      assert((error = Invariants.validate(value)(invariants)) === undefined, error?.message);
+      Invariants.assert(value)(invariants);
     }
     return {
       name,
