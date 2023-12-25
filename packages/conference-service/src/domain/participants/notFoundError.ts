4ded69f209b5df9b8e8d83b02cb566851c98c6aa
kimutyam <devkimura@gmail.com>
2023-12-25 15:37:32 +0900

    chore: this.nameに定義し忘れた

--- packages/conference-service/src/domain/participants/notFoundError.ts
+++ packages/conference-service/src/domain/participants/notFoundError.ts
@@ -3,5 +3,6 @@ import type { ConferenceId } from '../conference';
 export class RestrictionNotFoundError extends Error {
   constructor(public readonly conferenceId: ConferenceId) {
     super('参加制限が存在しません');
+    this.name = 'RestrictionNotFoundError';
   }
 }
