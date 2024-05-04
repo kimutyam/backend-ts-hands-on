6cc2d6119afd0f921739614414226a291803d0d6
kimutyam <devkimura@gmail.com>
2024-05-04 19:23:02 +0900

    fix: ResultAsyncにする

--- packages/book/src/aggregate/30_domain_event/domain/aggregateResolver.ts
+++ packages/book/src/aggregate/30_domain_event/domain/aggregateResolver.ts
@@ -1,5 +1,11 @@
+import type { ResultAsync } from 'neverthrow';
 import type { Aggregate } from './aggregate';
+import type { AggregateNotFoundError } from './aggregateNotFoundError';
 
-export interface AggregateResolver<AggregateId, T extends Aggregate<AggregateId, any>> {
-  resolveById(aid: AggregateId): T;
+export interface AggregateResolver<
+  AggregateId,
+  T extends Aggregate<AggregateId, any>,
+  E extends AggregateNotFoundError,
+> {
+  resolveById(id: AggregateId): ResultAsync<T, E>;
 }
