c9a6b30df4b3469c31a77fd34d82a0c4b359b754
kimutyam <devkimura@gmail.com>
2024-05-04 19:23:02 +0900

    fix: ResultAsyncにする

--- packages/book/src/aggregate/30_domain_event/domain/cartRepository.ts
+++ packages/book/src/aggregate/30_domain_event/domain/cartRepository.ts
@@ -2,8 +2,9 @@ import type { CustomerId } from '../../10_zod/domain/customer/customerId';
 import type { AggregateResolver } from './aggregateResolver';
 import type { Cart } from './cart';
 import type { CartEvent } from './cartEvent';
+import type { CartNotFoundError } from './cartNotFoundError';
 import type { EventStore } from './eventStore';
 
-export type CartResolver = AggregateResolver<CustomerId, Cart>;
+export type CartResolver = AggregateResolver<CustomerId, Cart, CartNotFoundError>;
 
 export type CartEventStore = EventStore<Cart, CartEvent>;
