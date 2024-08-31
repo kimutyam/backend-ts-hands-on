cc31d81a7bb8792c861ad842ac3ea6e05774754d
kimutyam <devkimura@gmail.com>
2024-08-31 15:36:53 +0900

    fix: 型推論の記述

--- packages/book/src/type_system/10_type_infer/main.ts
+++ packages/book/src/type_system/10_type_infer/main.ts
@@ -1,39 +1,39 @@
 {
-  let greeting = 'hello'; // string型と推論される
+  let greeting = 'hello'; // string型と推論されます
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  greeting = 10; // NG: string型と互換性のない型を再代入はすることはできない
-  greeting = 'hi'; // OK: string型の別の値を再代入が可能
+  greeting = 10; // NG: string型と互換性のない型の値を再代入
+  greeting = 'hi'; // OK: string型の別の値を再代入
 
-  let anything; // any型と推論される（明確な初期値がないため）
+  let anything; // 明確な初期値がないため、any型と推論されます
   // eslint-disable-next-line prefer-const
-  anything = 42; // any型のsubsetである型を代入できるが、型はanyのまま
+  anything = 42; // any型のsubsetである型を代入できますが、型は引き続きanyと推論されます
 
   console.log(greeting, anything);
 }
 
 {
-  const pi = 3.14; // piの型は3.14というリテラル型と推論される
-  const greeting = 'hello'; // greetingの型は"hello"というリテラル型と推論される
+  const pi = 3.14; // 型は3.14というリテラル型と推論されます
+  const greeting = 'hello'; // 型は"hello"というリテラル型と推論されます
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  greeting = 'world'; // NG: "hello" 以外は許容しない
+  greeting = 'world'; // NG: 値の再代入
 
   console.log(pi, greeting);
 }
 
 {
-  const person = { name: 'John', age: 30 }; // personの型は{name: string; age: number;}と推論される
-  person.age = 31; // 可能
+  const person = { name: 'John', age: 30 }; // 型は {name: string; age: number;} と推論されます
+  person.age = 31; // OK: オブジェクトのプロパティには再代入
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  person = { name: 'Doe', age: 40 }; // NG: 再代入はできない
+  person = { name: 'Doe', age: 40 }; // NG: オブジェクト自体を再代入
 
-  const numbers = [1, 2, 3]; // numbersの型はnumber[]と推論される
-  numbers.push(4); // 可能
+  const numbers = [1, 2, 3]; // numbersの型はnumber[]と推論されます
+  numbers.push(4); // OK: 配列の要素の追加
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  numbers = [4, 5, 6]; // NG: 再代入はできない
+  numbers = [4, 5, 6]; // NG: 配列自体の再代入
 
   console.log(person, numbers);
 }
