8402874c2667c8cd9dea49fb877b7e880a72df14
kimutyam <devkimura@gmail.com>
2024-08-31 16:32:34 +0900

    fix: 型注釈の表現修正

--- packages/book/src/type_system/20_type_annotation/item.ts
+++ packages/book/src/type_system/20_type_annotation/item.ts
@@ -6,6 +6,6 @@ interface Item {
 const item: Item = {
   itemId: 'Item-Id-A',
   price: 1_000,
-}; // 定義済みのオブジェクトの型を明示化する
+}; // 定義済みのオブジェクトの型を明示化します
 
 console.log(item);
--- packages/book/src/type_system/20_type_annotation/main.ts
+++ packages/book/src/type_system/20_type_annotation/main.ts
@@ -1,23 +1,21 @@
 {
   // eslint-disable-next-line @typescript-eslint/prefer-as-const, prefer-const
-  let greeting: 'hello' = 'hello'; // 型注釈をしなければ、string型と推論される
-  const pi = 3.14; // 型注釈をしなければ、数値リテラル型の3.14と推論される
-
-  console.log(greeting, pi);
+  let greeting: 'hello' = 'hello'; // 型注釈を入れなければ、string型と推論されます
+  console.log(greeting);
 }
 
 {
   // 数値、文字列、またはブール値に初期化された変数またはパラメーターの明示的な型宣言を禁止します。
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types, prefer-const
-  let greeting: string = 'hello'; // 型注釈をしなければ、string型と推論される
+  let greeting: string = 'hello'; // 型注釈を使わなくてもstring型と推論されますが、明示的にstring型の注釈を入れています
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
-  const pi: number = 3.14; // 型注釈をしなければ、数値リテラル型の3.14と推論される
+  const pi: number = 3.14; // 型注釈をしなければ、数値リテラル型の3.14と推論されるところを、明示的にnumber型の注釈を入れています
 
   console.log(greeting, pi);
 }
 
 {
-  let anything: number; // 型注釈をしなければ、any型と推論される
+  let anything: number; // 型注釈を入れなければ、any型と推論されます
   anything = 10;
   anything = 20;
 
