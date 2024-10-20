347ec9eb948a569a0db4b98bf8c5105e1a092389
kimutyam <devkimura@gmail.com>
2024-10-21 00:30:42 +0900

    fix: jestを消す

--- packages/book/eslint.config.mjs
+++ packages/book/eslint.config.mjs
@@ -1,19 +1,12 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import baseConfig from '../../eslint.config.mjs';
-import JestESLint from 'eslint-plugin-jest';
 
 const compat = new FlatCompat();
 
 export default [
   ...baseConfig,
-  ...compat.extends('plugin:jest/recommended', 'plugin:jest/style'),
   {
-    plugins: {
-      JestESLint,
-    },
     rules: {
-      'jest/consistent-test-it': ['error', { fn: 'it' }],
-      'jest/no-disabled-tests': 'off',
       'no-console': 'off',
     },
   },
