01b0092d328be16cc712c4db893b3c302461fd23
kimutyam <devkimura@gmail.com>
2024-12-11 23:41:20 +0900

    fix: Native ESMをやめたため、top level awaitの箇所を修正

--- packages/book/src/neverthrow/6_wrap_constructor/constructorAsync.ts
+++ packages/book/src/neverthrow/6_wrap_constructor/constructorAsync.ts
@@ -1,13 +1,19 @@
 import type { ResultAsync, Result } from 'neverthrow';
 import { okAsync, errAsync } from 'neverthrow';
 
-const okAsync1: ResultAsync<number, never> = okAsync(42);
-const okAwaited1: Result<number, never> = await okAsync1;
-okAwaited1.isOk(); // true
-okAwaited1.isErr(); // false
+async function main1() {
+  const okAsync1: ResultAsync<number, never> = okAsync(42);
+  const okAwaited1: Result<number, never> = await okAsync1;
+  okAwaited1.isOk(); // true
+  okAwaited1.isErr(); // false
+}
 
-const errAsync1: ResultAsync<never, string> = errAsync('Error');
-const errAwaited1: Result<never, string> = await errAsync1;
+async function main2() {
+  const errAsync1: ResultAsync<never, string> = errAsync('Error');
+  const errAwaited1: Result<never, string> = await errAsync1;
 
-errAwaited1.isOk(); // false
-errAwaited1.isErr(); // true
+  errAwaited1.isOk(); // false
+  errAwaited1.isErr(); // true
+}
+
+export { main1, main2 };
