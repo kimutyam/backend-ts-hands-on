d16d36954ca57a2f8e2bf5979e2960690f1abaf2
kimutyam <devkimura@gmail.com>
2025-02-21 15:04:29 +0900

    fix: 8章のコードをコピー

---
+++ packages/book/src/9_always_valid_domain_model/50/cartItem.ts
@@ -0,0 +1,36 @@
+import type { Price } from './price.js';
+import type { ProductId } from './productId.js';
+import { Quantity } from './quantity.js';
+
+interface CartItem {
+  readonly productId: ProductId;
+  readonly quantity: Quantity;
+  readonly price: Price;
+}
+
+const add =
+  (quantity: Quantity, price: Price) =>
+  (item: CartItem): CartItem => ({
+    ...item,
+    quantity: Quantity.build(item.quantity + quantity),
+    price,
+  });
+
+const calculateTotal = ({ price, quantity }: CartItem): number => price * quantity;
+
+const buildSingle = (productId: ProductId, price: Price): CartItem => ({
+  productId,
+  quantity: Quantity.build(1),
+  price,
+});
+
+const identify = (x: CartItem, y: CartItem): boolean => x.productId === y.productId;
+
+const CartItem = {
+  add,
+  calculateTotal,
+  buildSingle,
+  identify,
+} as const;
+
+export { CartItem };
---
+++ packages/book/src/9_always_valid_domain_model/50/domainEventId.ts
@@ -0,0 +1,32 @@
+import assert from 'node:assert';
+import { decodeTime, isValid, ulid } from 'ulidx';
+import type { Brand } from './brand.js';
+
+type DomainEventId = string & Brand<'DomainEventId'>;
+
+const equals = (a: DomainEventId, b: DomainEventId): boolean => a === b;
+
+const assertDomainEventId = (value: DomainEventId): void => {
+  assert(isValid(value), 'ULIDで指定ください');
+};
+
+const build = (value: string): DomainEventId => {
+  const v = value as DomainEventId;
+  assertDomainEventId(v);
+  return v;
+};
+
+// 乱数生成器のシード
+const SEED = 123;
+const generate = (): DomainEventId => build(ulid(SEED));
+
+const getTimestamp = (id: DomainEventId): number => decodeTime(id);
+
+const DomainEventId = {
+  build,
+  generate,
+  equals,
+  getTimestamp,
+} as const;
+
+export { DomainEventId };
