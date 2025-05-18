c8219f3950f1cd2436536a0a41611b3aeb7c8263
kimutyam <devkimura@gmail.com>
2025-05-18 23:15:34 +0900

    fix: テストケース追加

--- packages/hono-drizzle/src/adaptor/serverSide/postgresql/__tests__/findCartById.test.ts
+++ packages/hono-drizzle/src/adaptor/serverSide/postgresql/__tests__/findCartById.test.ts
@@ -3,6 +3,7 @@ import assert from 'node:assert';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 import { Aggregate } from '../../../../domain/aggregate.js';
+import { CartNotFoundError } from '../../../../domain/cart/cartNotFoundError.js';
 import { Quantity } from '../../../../domain/cart/quantity.js';
 import { CustomerId } from '../../../../domain/customer/customerId.js';
 import { Price } from '../../../../domain/product/price.js';
@@ -22,6 +23,7 @@ describe('FindCartById', () => {
 
   const customerId1 = CustomerId.generate();
   const customerId2 = CustomerId.generate();
+  const customerId3 = CustomerId.generate();
 
   const productId1 = ProductId.generate();
 
@@ -40,6 +42,10 @@ describe('FindCartById', () => {
       },
       {
         customerId: customerId2,
+        name: 'Customer2',
+      },
+      {
+        customerId: customerId3,
         name: 'Customer2',
       },
     ]);
@@ -66,7 +72,23 @@ describe('FindCartById', () => {
     await pool.end();
   });
 
-  test('カートアイテムが空の場合', async () => {
+  test('登録済みのカートで索引できる', async () => {
+    const result = await findCartById(customerId1);
+    assert(result.isOk());
+    expect(result.value).toStrictEqual({
+      aggregateId: customerId1,
+      sequenceNumber: Aggregate.InitialSequenceNumber,
+      cartItems: [
+        {
+          productId: productId1,
+          price: Price.parse(1_000),
+          quantity: Quantity.parse(2),
+        },
+      ],
+    });
+  });
+
+  test('カートアイテムが空の場合でも索引できる', async () => {
     const result = await findCartById(customerId2);
     assert(result.isOk());
     expect(result.value).toStrictEqual({
@@ -74,5 +96,11 @@ describe('FindCartById', () => {
       sequenceNumber: 5,
       cartItems: [],
     });
+  });
+  test('カートが存在しない場合はエラーとなる', async () => {
+    const result = await findCartById(customerId3);
+    assert(result.isErr());
+    expect(result.error).toBeInstanceOf(CartNotFoundError);
+    expect(result.error.customerId).toBe(customerId3);
   });
 });
