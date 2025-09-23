201e2f0bc797dfa0938286ea59bf1e7da997a182
kimutyam <devkimura@gmail.com>
2025-09-23 17:49:14 +0900

    fix: 環境変数を利用

--- packages/hono-drizzle/drizzle.config.ts
+++ packages/hono-drizzle/drizzle.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 
-const { DATABASE_URL } = process.env;
-if (DATABASE_URL === undefined) {
-  throw new Error('DATABASE_URL is not defined');
-}
-console.log('DATABASE_URL:', DATABASE_URL);
-
 export default defineConfig({
   dialect: 'postgresql',
   schema: './dist-drizzle-kit/adapter/secondary/db/rdb/schema/*.sql.js',
   out: './drizzle',
   dbCredentials: {
-    url: DATABASE_URL,
+    url: process.env['DATABASE_URL']!,
   },
 });
--- packages/hono-drizzle/src/adapter/secondary/db/rdb/__tests__/helper/db.ts
+++ packages/hono-drizzle/src/adapter/secondary/db/rdb/__tests__/helper/db.ts
@@ -1,7 +1,5 @@
 import { Db } from '../../db.js';
 
-const testDb = Db.build(
-  'postgresql://postgres:postgres@localhost:5433/test_db',
-);
+const testDb = Db.build(process.env['DATABASE_URL']!);
 
 export { testDb };
