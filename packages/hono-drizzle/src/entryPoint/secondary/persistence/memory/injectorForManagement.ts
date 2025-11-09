5f9a775221d3126f28d980ed2877df360c8ebb40
kimutyam <devkimura@gmail.com>
2025-11-09 22:58:38 +0900

    fix: UserAccountのサンプルコードを削除

--- packages/hono-drizzle/src/entryPoint/secondary/persistence/memory/injectorForManagement.ts
+++ packages/hono-drizzle/src/entryPoint/secondary/persistence/memory/injectorForManagement.ts
@@ -1,17 +1,15 @@
 import type { Injector } from 'typed-inject';
 
 import { ProductRepository } from '../../../../adapter/secondary/persistence/memory/productRepository.js';
-import { UserAccountRepository } from '../../../../adapter/secondary/persistence/memory/userAccountRepository.js';
 import { StoreProductEvent } from '../../../../app/port/secondary/persistence/productEventStore.js';
-import { FindUserAccountById } from '../../../../app/port/secondary/persistence/userAccountRepository.js';
 import type { PersistencePortInjector } from '../injectorForManagement.js';
 
 const create = (rootInjector: Injector): PersistencePortInjector => {
-  const userAccountRepository = UserAccountRepository.create();
   const productRepository = ProductRepository.create();
-  return rootInjector
-    .provideValue(FindUserAccountById.token, userAccountRepository.findById)
-    .provideValue(StoreProductEvent.token, productRepository.store);
+  return rootInjector.provideValue(
+    StoreProductEvent.token,
+    productRepository.store,
+  );
 };
 
 const MemoryAdaptorInjector = {
