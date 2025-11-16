0c09bdb93971844e38f06fdf3393bb631364ffb0
kimutyam <devkimura@gmail.com>
2025-11-16 17:14:59 +0900

    feat: エラーメッセージを設定する

---
+++ packages/book/src/ch5/ex5501/__tests__/employeeSchema.test.ts
@@ -0,0 +1,41 @@
+import { assert, expect } from 'vitest';
+
+import { employeeSchema } from '../employeeSchema.js';
+
+describe('safeParse', () => {
+  it('名前と年齢のバリデーションエラー', () => {
+    const result = employeeSchema.safeParse({
+      name: 100_000_000_000,
+      age: 104.5,
+    });
+    assert(!result.success);
+    expect(result.error.issues).toStrictEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: '名前は文字列で指定してください',
+        }),
+        expect.objectContaining({
+          message: '年齢は整数で指定してください',
+        }),
+        expect.objectContaining({
+          message: '年齢は60以下で指定してください',
+        }),
+      ]),
+    );
+  });
+
+  it('必須項目のバリデーションエラー', () => {
+    const result = employeeSchema.safeParse({});
+    assert(!result.success);
+    expect(result.error.issues).toStrictEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: '名前は文字列で指定してください',
+        }),
+        expect.objectContaining({
+          message: '年齢は数値で指定してください',
+        }),
+      ]),
+    );
+  });
+});
---
+++ packages/book/src/ch5/ex5501/employeeSchema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const nameSchema = z
+  .string('名前は文字列で指定してください')
+  .min(1, '1文字以上で指定ください')
+  .max(10, `名前は10文字以内で指定ください`);
+
+const employeeSchema = z.object({
+  name: nameSchema,
+  age: z
+    .number('年齢は数値で指定してください')
+    .min(10, `年齢は10以上で指定してください`)
+    .max(60, `年齢は60以下で指定してください`)
+    .int('年齢は整数で指定してください'),
+});
+
+export { employeeSchema };
